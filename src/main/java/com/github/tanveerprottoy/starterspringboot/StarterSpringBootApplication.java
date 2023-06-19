5e3c5a9ecd500651c4d10cca6945d1e5835dcd36
tanveerprottoy <tanveer.prottoy@gmail.com>
2023-06-19 23:57:04 +0600

    add new configs

--- src/main/java/com/github/tanveerprottoy/starterspringboot/StarterSpringBootApplication.java
+++ src/main/java/com/github/tanveerprottoy/starterspringboot/StarterSpringBootApplication.java
@@ -2,11 +2,20 @@ package com.github.tanveerprottoy.starterspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StarterSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StarterSpringBootApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(StarterSpringBootApplication.class, args);
+		printAllBeans(applicationContext);
+	}
+
+	private static void printAllBeans(ApplicationContext applicationContext) {
+		String[] allBeanNames = applicationContext.getBeanDefinitionNames();
+		for(String beanName : allBeanNames) {
+			System.out.println(beanName);
+		}
 	}
 }
